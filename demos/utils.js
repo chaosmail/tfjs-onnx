e98b3dd928de72ee3d6abf235d16428c90b944ad
Christoph Koerner <office@chaosmail.at>
2018-05-13 11:57:37 +0200

    update demo code

--- demos/utils.js
+++ demos/utils.js
@@ -1,7 +1,28 @@
-function loadImageData(img, shape) {
-  const pixels = tf.fromPixels(img);
-  const data = tf.image.resizeBilinear(pixels, shape);
-  return data.expandDims().cast('float32');
+
+function getImageData(player, options) {
+  options = options || {};
+  options.shape = options.shape || null;
+  options.gray = options.gray || false;
+  options.crop = options.crop || false;
+
+  return tf.tidy(() => {
+
+    const img = tf.fromPixels(player);
+
+    const croppedImg = options.crop ?
+      cropImage(img) : img;
+
+    const resizedImg = options.shape ?
+      tf.image.resizeBilinear(croppedImg, options.shape) :
+      croppedImg;
+
+    const grayImg = options.gray ?
+      rgbToGrayscale(resizedImg) :
+      resizedImg;
+
+    // Convert to float and add batch dimension
+    return grayImg.cast('float32').expandDims();
+  });
 }
 
 function displayImage(data, elemId) {
@@ -22,12 +43,25 @@ function displayLabel(probs, labels, elemId) {
 }
 
 // TODO port back to tfjs
+// @src https://github.com/tensorflow/tensorflow/blob/r1.8/tensorflow/python/ops/image_ops_impl.py#L1255
 function rgbToGrayscale(input) {
-  // @src https://github.com/tensorflow/tensorflow/blob/r1.8/tensorflow/python/ops/image_ops_impl.py#L1255
-  const rgbWeights = tf.tensor1d([0.2989, 0.5870, 0.1140]);
-  const floatInput = input.cast('float32');
-  const grayImage = floatInput.mul(rgbWeights).sum(-1);
-  return grayImage.expandDims(input.shape.length - 1);
+  return tf.tidy(() => {
+    const rgbWeights = tf.tensor1d([0.2989, 0.5870, 0.1140]);
+    const floatInput = input.cast('float32');
+    const grayImage = floatInput.mul(rgbWeights).sum(-1);
+    return grayImage.expandDims(input.shape.length - 1);
+  });
+}
+
+// TODO port back to tfjs
+// @src https://github.com/tensorflow/tfjs-examples/blob/master/webcam-transfer-learning/webcam.js#L56
+function cropImage(img) {
+  const size = Math.min(img.shape[0], img.shape[1]);
+  const centerHeight = img.shape[0] / 2;
+  const beginHeight = centerHeight - (size / 2);
+  const centerWidth = img.shape[1] / 2;
+  const beginWidth = centerWidth - (size / 2);
+  return img.slice([beginHeight, beginWidth, 0], [size, size, img.shape[2]]);
 }
 
 async function getTopKClasses(logits, topK) {
