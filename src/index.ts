7fa7c54b29fb9df59b23937b93d6b2f2f5380437
Frithjof Winkelmann <frithjof97@web.de>
2021-10-18 23:34:31 +0200

    Add method to load model from array buffer (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import {loadModel} from './model';
+import { loadModel, loadModelFromBuffer } from './model';
 import * as util from './util';
 
 export * from '@tensorflow/tfjs';
 
 export const onnx = {
   'util': util,
-  'loadModel': loadModel
+  'loadModel': loadModel,
+  'loadModelFromBuffer': loadModelFromBuffer
 };
